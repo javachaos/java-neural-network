0d7ac825ea3c323689da37a9fe902354423b8496
Fred Laderoute <fredladeroute@gmail.com>
2013-08-16 07:58:25 -0400

    Added INetwork

---
+++ core/src/main/java/com/neuralnetwork/core/INetwork.java
@@ -0,0 +1,14 @@
+package com.neuralnetwork.core;
+
+import com.neuralnetwork.shared.layers.ILattice;
+
+/**
+ * Represents an Artificial neural network of INodes.
+ * 
+ * @author fredladeroute
+ *
+ */
+public interface INetwork extends ILattice {
+    
+
+}
